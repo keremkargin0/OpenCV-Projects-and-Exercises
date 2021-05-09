22ec6a1673abc34edd8f84d088cf7f13db3f7c8d
Kerem Kargın <77541962+keremkargin0@users.noreply.github.com>
2021-05-09 14:01:47 +0300

    Update 01.Face Detection from Webcam.py

--- Applications/01.Face Detection from Webcam.py
+++ Applications/01.Face Detection from Webcam.py
@@ -1,1 +1,24 @@
+import cv2
 
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier("frontalface.xml")
+
+while 1:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray_frame, 1.3, 7)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    cv2.imshow("Video", frame)
+
+    if cv2.waitKey(20) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
